3e8c5ccdf128cda73b01c68a5d8f7e38d88598c5
YousefAlhawi <cscyber17@gmail.com>
2026-04-25 23:37:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 }
 
-MAX_CRAWL_DEPTH = 2  # متطلب المشروع: العمق = 2 
+MAX_CRAWL_DEPTH = 2 
 CRAWL_DELAY = 1.0
-OUTPUT_FOLDER = "results" # [cite: 60]
+OUTPUT_FOLDER = "results" 
 
-# كلمات البحث عن الأخبار كما في متطلبات المشروع [cite: 40, 42, 43, 44, 45]
 NEWS_KEYWORDS = ["news", "article", "headline", "story", "أخبار", "فعاليات"]
