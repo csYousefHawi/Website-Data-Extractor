d6555f6083fb26c570a7e9133246ea533cf6bf2c
YousefAlhawi <cscyber17@gmail.com>
2026-04-25 23:27:06 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+}
+
+MAX_CRAWL_DEPTH = 2  # متطلب المشروع: العمق = 2 
+CRAWL_DELAY = 1.0
+OUTPUT_FOLDER = "results" # [cite: 60]
+
+# كلمات البحث عن الأخبار كما في متطلبات المشروع [cite: 40, 42, 43, 44, 45]
+NEWS_KEYWORDS = ["news", "article", "headline", "story", "أخبار", "فعاليات"]
